34867bc6b59cc35ce16d592fc9b1f296f51051f7
Arseniy <seny_2978@mail.ru>
2021-12-01 20:45:15 +0300

    okey

---
+++ js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let number = 5;
+const leftBorderWidth = 1;
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false,
+};
+console.log(obj.name);
+console.log(obj['age'])
+let array = ['plum.png', 'orange.jpg', 'apple.bmp'];
+const result = confirm("Are you okey?")
+
+
+const category = "toys";
+console.log(`https://someurl.com/${category}/5`)
+console.log('xdxd')
