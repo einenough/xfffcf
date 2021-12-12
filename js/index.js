2105e5123be54270e2e5a8a2064a76568c2eec50
Arseniy <seny_2978@mail.ru>
2021-12-12 18:59:44 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,241 +1,453 @@
-"use strict";
-// let number = 5;
-// const leftBorderWidth = 1;
-// const obj = {
-//     name: "John",
-//     age: 25,
-//     isMarried: false,
-// };
-// console.log(obj.name);
-// console.log(obj['age'])
-// let array = ['plum.png', 'orange.jpg', 'apple.bmp'];
-// const result = confirm("Are you okey?")
+// // "use strict";
+// // // let number = 5;
+// // // const leftBorderWidth = 1;
+// // // const obj = {
+// // //     name: "John",
+// // //     age: 25,
+// // //     isMarried: false,
+// // // };
+// // // console.log(obj.name);
+// // // console.log(obj['age'])
+// // // let array = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// // // const result = confirm("Are you okey?")
 
 
-// const category = "toys";
-// console.log(`https://someurl.com/${category}/5`)
-// console.log('xdxd')
-// console.log('123x');
+// // // const category = "toys";
+// // // console.log(`https://someurl.com/${category}/5`)
+// // // console.log('xdxd')
+// // // console.log('123x');
 
 
-// const numbeOfFilms = prompt('How many times you watch this film?')
-// const personalMovieDB = {
-//     count: numbeOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: {},
-//     privat: false
-// };
-// const a = prompt('What last film you watch?'),
-//       b = prompt('You opinion?'),
-//       c = prompt('What last film you watch?'),
-//       d = prompt('You opinion?');
+// // // const numbeOfFilms = prompt('How many times you watch this film?')
+// // // const personalMovieDB = {
+// // //     count: numbeOfFilms,
+// // //     movies: {},
+// // //     actors: {},
+// // //     genres: {},
+// // //     privat: false
+// // // };
+// // // const a = prompt('What last film you watch?'),
+// // //       b = prompt('You opinion?'),
+// // //       c = prompt('What last film you watch?'),
+// // //       d = prompt('You opinion?');
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+// // // personalMovieDB.movies[a] = b;
+// // // personalMovieDB.movies[c] = d;
 
-// if (4 == 9){
-//     console.log('ok')
-// } else {
-//     console.log('Error!')
-// };
-
-
-// const num = 50;
-// if (num < 49) {
-//     console.log('idc')
-// } else if ( num > 100){ 
-//     console.log('A lot')
-// } else {
-//     console.log('Ok')
-// }
-
-// (num === 50) ? console.log('ok') : console.log('a lot');
-
-// const yum = 50;
+// // // if (4 == 9){
+// // //     console.log('ok')
+// // // } else {
+// // //     console.log('Error!')
+// // // };
 
 
-// switch (yum) {
-//     case 49:
-//         console.log('Neverno');
-//         break;
-//     case 100:
-//         console.log('Neverno');
-//         break;
-//     case 50:
-//         console.log('its ok');
-//         break;
-//     default:
-//         console.log('everything is not right');
-//         break; 
+// // // const num = 50;
+// // // if (num < 49) {
+// // //     console.log('idc')
+// // // } else if ( num > 100){ 
+// // //     console.log('A lot')
+// // // } else {
+// // //     console.log('Ok')
+// // // }
+
+// // // (num === 50) ? console.log('ok') : console.log('a lot');
+
+// // // const yum = 50;
+
+
+// // // switch (yum) {
+// // //     case 49:
+// // //         console.log('Neverno');
+// // //         break;
+// // //     case 100:
+// // //         console.log('Neverno');
+// // //         break;
+// // //     case 50:
+// // //         console.log('its ok');
+// // //         break;
+// // //     default:
+// // //         console.log('everything is not right');
+// // //         break; 
     
-// }
+// // // }
 
-// let bum = 50;
+// // // let bum = 50;
 
-// while (bum < 55){
-//     console.log(bum);
-//     bum++;
-// }
+// // // while (bum < 55){
+// // //     console.log(bum);
+// // //     bum++;
+// // // }
 
-// do {
-//     console.log(bum);
-//     bum++;
-// } 
-// while (bum < 55);
+// // // do {
+// // //     console.log(bum);
+// // //     bum++;
+// // // } 
+// // // while (bum < 55);
 
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         break;
-//     }
-//     console.log(i);
-// }
-// const numbeOfFilms = prompt('How many times you watch this film?');
-// const personalMovieDB = {
-//     count: numbeOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: {},
-//     privat: false
-// };
+// // // for (let i = 1; i < 10; i++) {
+// // //     if (i === 6) {
+// // //         break;
+// // //     }
+// // //     console.log(i);
+// // // }
+// // // const numbeOfFilms = prompt('How many times you watch this film?');
+// // // const personalMovieDB = {
+// // //     count: numbeOfFilms,
+// // //     movies: {},
+// // //     actors: {},
+// // //     genres: {},
+// // //     privat: false
+// // // };
 
 
-// for ( let i = 0; i < 2; i++){
-//     const a = prompt ('Question'),
-//           b = prompt('Second Question');
+// // // for ( let i = 0; i < 2; i++){
+// // //     const a = prompt ('Question'),
+// // //           b = prompt('Second Question');
     
-//     if (a != null && b != null && a !='' && b!='' && a.length < 50 && b.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done!');
+// // //     if (a != null && b != null && a !='' && b!='' && a.length < 50 && b.length < 50) {
+// // //         personalMovieDB.movies[a] = b;
+// // //         console.log('done!');
 
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
+// // //     } else {
+// // //         console.log('error');
+// // //         i--;
+// // //     }
     
  
-// }
-// console.log(personalMovieDB);
-// if (personalMovieDB.count < 10){
-//     console.log('hmm');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
-//     console.log('wowow');
-// } else if (personalMovieDB.count > 30){
-//     console.log('Kinoman');
-// } else {
-//     console.log('Error');
-// };
-// console.log(personalMovieDB);
+// // // }
+// // // console.log(personalMovieDB);
+// // // if (personalMovieDB.count < 10){
+// // //     console.log('hmm');
+// // // } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+// // //     console.log('wowow');
+// // // } else if (personalMovieDB.count > 30){
+// // //     console.log('Kinoman');
+// // // } else {
+// // //     console.log('Error');
+// // // };
+// // // console.log(personalMovieDB);
 
-// let i = 0;
-// do {
-//    const f = prompt('quest'),
-//          v = prompt('2ndquest');
-//     i++;
-// } while( i < 2 ); 
-// let num = 34;
-// function showFirstMessage(text){
-//     console.log(text);
-//      num = 20;
-// }
-// showFirstMessage('Hello World!');
-// console.log(num);
+// // // let i = 0;
+// // // do {
+// // //    const f = prompt('quest'),
+// // //          v = prompt('2ndquest');
+// // //     i++;
+// // // } while( i < 2 ); 
+// // // let num = 34;
+// // // function showFirstMessage(text){
+// // //     console.log(text);
+// // //      num = 20;
+// // // }
+// // // showFirstMessage('Hello World!');
+// // // console.log(num);
 
-// function rec (){
-//     let bum = 70;
-//     return bum;
-// }
-// const  showBum = rec();
-// let boom = function(){
-//     console.log('Hee')
-// };
-// let calc = (a,b) => a + b;// в одну строчку можно так.
-// let calcу = (a,b) => {
-//     console.log('xdxd');
-//     return a+b;
-// };// в несколько строчек - так.
+// // // function rec (){
+// // //     let bum = 70;
+// // //     return bum;
+// // // }
+// // // const  showBum = rec();
+// // // let boom = function(){
+// // //     console.log('Hee')
+// // // };
+// // // let calc = (a,b) => a + b;// в одну строчку можно так.
+// // // let calcу = (a,b) => {
+// // //     console.log('xdxd');
+// // //     return a+b;
+// // // };// в несколько строчек - так.
 
-// let key = 17;
-// let hr = (a) => a + key;
-// console.log(hr(13));
+// // // let key = 17;
+// // // let hr = (a) => a + key;
+// // // console.log(hr(13));
 
-// let str = 'hahaha'
-// console.log(str.toLocaleUpperCase());
-// const logged = 'Key World';
-// console.log(logged.slice(4,9));
-// console.log(logged.substr(5,5));
-// const vum = 12.2;
-// console.log(Math.round(vum));
-// const vest = '12.2px';
-// console.log(parseInt(vest));
+// // // let str = 'hahaha'
+// // // console.log(str.toLocaleUpperCase());
+// // // const logged = 'Key World';
+// // // console.log(logged.slice(4,9));
+// // // console.log(logged.substr(5,5));
+// // // const vum = 12.2;
+// // // console.log(Math.round(vum));
+// // // const vest = '12.2px';
+// // // console.log(parseInt(vest));
 
-let numberOfFilms;
-const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
-
-
-function start () {
-    numberOfFilms = +prompt('Any question?');
-
-    while (numberOfFilms== '' || numberOfFilms== null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('Any question?');
-
-    }
-}
+// // // let numberOfFilms;
+// // // const personalMovieDB = {
+// // //         count: numberOfFilms,
+// // //         movies: {},
+// // //         actors: {},
+// // //         genres: [],
+// // //         privat: false
+// // //     };
 
 
+// // // function start () {
+// // //     numberOfFilms = +prompt('Any question?');
+
+// // //     while (numberOfFilms== '' || numberOfFilms== null || isNaN(numberOfFilms)){
+// // //         numberOfFilms = +prompt('Any question?');
+
+// // //     }
+// // // }
 
 
-function rememberMyFilms () {
-    for ( let i = 0; i < 2; i++){
-    const a = prompt ('Question'),
-          b = prompt('Second Question');
+
+
+// // // function rememberMyFilms () {
+// // //     for ( let i = 0; i < 2; i++){
+// // //     const a = prompt ('Question'),
+// // //           b = prompt('Second Question');
     
-    if (a != null && b != null && a !='' && b!='' && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done!');
+// // //     if (a != null && b != null && a !='' && b!='' && a.length < 50 && b.length < 50) {
+// // //         personalMovieDB.movies[a] = b;
+// // //         console.log('done!');
 
-    } else {
-        console.log('error');
-        i--;
-     }
-    }
-};
-
-
-
-function detectLevelPerson () {
-    if (personalMovieDB.count < 10){
-            console.log('hmm');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
-            console.log('wowow');
-        } else if (personalMovieDB.count > 30){
-            console.log('Kinoman');
-        } else {
-            console.log('Error');
-        };
-
-};
-function showMyDB (){
-    if (personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    }
-};
-
-showMyDB();
+// // //     } else {
+// // //         console.log('error');
+// // //         i--;
+// // //      }
+// // //     }
+// // // };
 
 
-function writeYourGenres (){
-    for ( let i = 1; i <= 3; i++){
-        const genre = prompt(`Your favorite genres under number${i}?`)
-        personalMovieDB.genres[i-1] = genre;
 
-    }
-};
+// // // function detectLevelPerson () {
+// // //     if (personalMovieDB.count < 10){
+// // //             console.log('hmm');
+// // //         } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+// // //             console.log('wowow');
+// // //         } else if (personalMovieDB.count > 30){
+// // //             console.log('Kinoman');
+// // //         } else {
+// // //             console.log('Error');
+// // //         };
 
-writeYourGenres(); 
+// // // };
+// // // function showMyDB (){
+// // //     if (personalMovieDB.privat == false){
+// // //         console.log(personalMovieDB);
+// // //     }
+// // // };
+
+// // // showMyDB();
+
+
+// // // function writeYourGenres (){
+// // //     for ( let i = 1; i <= 3; i++){
+// // //         const genre = prompt(`Your favorite genres under number${i}?`)
+// // //         personalMovieDB.genres[i-1] = genre;
+
+// // //     }
+// // // };
+
+// // // writeYourGenres(); 
+
+// // const options = {
+// //     name: 'Arseniy',
+// //     surname: 'Abramov',
+// //     age: 19,
+// //     colors: {
+// //         border: 'red',
+// //         bg: 'black'
+// //     },
+// //     makeTest: function(){
+// //         console.log('Test')
+// //     }
+// // };
+// // options.makeTest();
+
+// // let counter = 0;
+
+// // for ( let key in options){
+// //     if (typeof(options[key]) === 'object') {
+// //         for (let i in options[key]){
+// //             console.log(`Свойство ${i}, имеет значение ${options[key][i]}`);
+// //             counter++;
+// //         }
+// //     } else {
+// //         console.log(`Свойство ${key}, имеет значение ${options[key]}`);
+// //         counter++;
+// //     }
+// // };
+// // console.log(counter);
+
+// // console.log(Object.keys(options));
+
+
+// // const hello = {
+// //     name: ' Ars',
+// //     surname: ' Abr',
+// //     age: 19
+// // };
+// // for ( let mey in hello){
+// //     console.log(hello[mey]);
+// // }
+
+// // const {border, bg} = options.colors;
+// // console.log(border);
+
+// // const arr = [1, 2, 3, 4];
+// // arr.pop();
+// // console.log(arr);
+// // arr.push(10);
+// // console.log(arr);
+
+// // for( let i = 0; i < arr.length; i++){
+// //     console.log(arr[i])
+// // }
+// // for(let value of arr){
+// //     console.log(value);
+// // };
+
+
+// // // arr.forEach(function(item, i, arr) {
+// // //     console.log(`${i}:${item + 1} inside ${arr}`);
+// // // })
+
+// // // const str = prompt('');
+// // // const productss = str.split(',');
+// // // console.log(productss.join('; '));
+
+// // // const ghostArray = [1, 2, 3, 7, 10, 14, 11, 17, 21, 16];
+// // // ghostArray.sort(compareNum)
+// // // console.log(ghostArray);
+
+// // // function compareNum(a, b){
+// // //     console.log('a', a);
+// // //     console.log('b', b);
+// // //     return a - b;
+// // // }
+// // function copy (mainObj) {
+// //     let komCopy = {};
+// //     let key;
+// //     for ( key in mainObj){
+// //         komCopy[key] = mainObj[key];
+// //     }
+// //     return komCopy;
+// // }
+// // const numbers = {
+// //     a:2,
+// //     b:5,
+// //     c:{
+// //         x:7,
+// //         y:4
+// //     }
+// // }
+
+// // const newNumbers = copy(numbers);
+
+// // newNumbers.a = 10;
+// // console.log(newNumbers);
+// // console.log(numbers);
+// // const motivate = {
+// //     d:20,
+// //     j:17
+// // };
+// // console.log(Object.assign(numbers,motivate));
+
+
+
+// // let str = 'some';
+// // let strObj = new String(str);
+// // console.log(typeof(str));
+// // console.log(typeof(strObj));
+
+// // const soldier = {
+// //     health: 400,
+// //     armor: 100,
+// //     sayHello: function() {
+// //         console.log("hello");
+// //     }
+// // };
+// // let john = Object.create(soldier);
+// // // john = {
+// // //     health:100,
+// // //     brew:200
+// // // };
+
+
+// // // const john = {
+// // //     health: 100
+// // // }
+// // // Object.setPrototypeOf(john, soldier); лучше то что выше
+
+// // // john.__proto__ = soldier; устарело
+
+// // console.log(john.armor);
+// // console.log(john.sayHello);
+// // console.log(john.brew);
+
+// const personalMovieDB = {
+//     count : 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = +prompt ('Сколько фильмов вы уже посмотрели?');
+
+//         while(personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
+//         }
+//     },
+//     rememberMyFilms: function(){
+//         for ( let i = 0; i<2; i++){
+//             const a=prompt('Один из последних просмотренных фильмов?'),
+//                   b=prompt('На сколько оцените его?');
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done!');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//         }
+//     },
+//     detectLevelPerson: function () {
+//         if (personalMovieDB.count < 10){
+//                 console.log('Просмотрено довольно мало фильмов');
+//             } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//                 console.log('Вы классный зритель');
+//             } else if (personalMovieDB.count >= 30){
+//                 console.log('Вы киноман');
+//             } else {
+//                 console.log('Error');
+//             }
+//     },
+//     showMyDB: function (hidden){
+//         if (!hidden){
+//             console.log(personalMovieDB);
+//         }
+
+//     },
+//     toggleVisibleMyDB: function(){
+//         if (personalMovieDB.privat){
+//             personalMovieDB.private = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function (){
+//         for( let i = 0; i<=3; i++){
+//             let genre = prompt(`Ваш любимый жанр под номером${i}`);
+//             if (genre === "" || genre == 'null'){
+//                 console.log('Вы ввели некорректные данные или не ввели их вовсе');
+//                 i--;
+//             } else{
+//                 personalMovieDB.genres[i - 1] = genre;
+
+//             }
+            
+//         }
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//         })
+//     }
+// };
+// И запинается на лжи &&
+// ИЛИ запинается на правде ||
+
+console.log(([] +false - true));
+
+console.log(([] + 1 +2));
+console.log((1 && 2));
+console.log((1 && 0 && 2));
+console.log(( null || 1));
